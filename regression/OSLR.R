9e252cedbc886608228f1bd817b268af8b31884d
maayanktyagi <maayanktyagi@gmail.com>
2016-07-09 23:56:24 +0530

    edited Oslr

--- regression/OSLR.R
+++ regression/OSLR.R
@@ -1,1 +1,6 @@
-
+dataset <- read.csv(dataset_name)
+fit <- lm(p~., dataset) #p is variable to be predicted
+summary(fit)
+predictions <- predict(p,datset)
+ans <- mean((dataset$p - predictions)^2)
+print(ans)
